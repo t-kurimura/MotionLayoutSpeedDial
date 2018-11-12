857e0657a9a27632e439e019e81dd22d5516117d
t-kurimura <takahisa.kurimura@eure.jp>
2018-11-13 04:13:54 +0900

    make all MotionLayout of SpeedDial - FloatingActionButton

---
+++ app/src/main/java/com/t_kurimura/motion_layout_speed_dial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.t_kurimura.motion_layout_speed_dial
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
